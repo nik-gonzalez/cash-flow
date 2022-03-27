375e6cbac5c10dfa32c69210cf75794b965f0830
Nik Gonzalez <NicanorAntonio.Gonzalez@alliedtelesis.com>
2022-03-27 14:41:38 +0800

    simplify code

--- api/interfaces.go
+++ api/interfaces.go
@@ -1,11 +1,22 @@
 package api
 
-import "cash-flow-service/models"
+import (
+	"cash-flow-service/models"
+	"github.com/gin-gonic/gin"
+)
 
-type Service interface {
+type IService interface {
 	GetCashFlows() ([]models.CashFlow, error)
 	AddCashFlow(cashFlow models.CashFlow) error
 	UpdateCashFlow(cashFlow models.CashFlow) error
 	GetCashFlow(id string) (*models.CashFlow, error)
 	DeleteCashFlow(id string) error
+}
+
+type IHandler interface {
+	GetCashFlows(ctx *gin.Context)
+	AddCashFlow(c *gin.Context)
+	UpdateCashFlows(c *gin.Context)
+	GetCashFlow(c *gin.Context)
+	DeleteCashFlow(c *gin.Context)
 }
--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 	service := memory.InMemoryService{}
-	handler := handlers.NewWithService(service)
+	handler := handlers.NewHandler(service)
 	router := gin.Default()
 	router.GET("/cash-flows", handler.GetCashFlows)
 	router.GET("/cash-flows/:id", handler.GetCashFlow)
-	router.POST("/cash-flows", handler.AddCashFlows)
+	router.POST("/cash-flows", handler.AddCashFlow)
 	router.PUT("/cash-flows", handler.UpdateCashFlows)
 	router.DELETE("/cash-flows/:id", handler.DeleteCashFlow)
 	router.Run("localhost:8080")
